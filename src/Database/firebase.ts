98d6eaa8691edddb41d67ea00ffdc0a710bc8f24
raxitsanghani <raxitsanghani@gmail.com>
2025-12-30 15:00:59 +0530

    feat: Initialize Firebase services with environment variables and update build configuration.

--- src/Database/firebase.ts
+++ src/Database/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBXgZe6FPfSyKoz8m3Uqrixcz-M0Fejz58",
-  authDomain: "nexura-a5e21.firebaseapp.com",
-  projectId: "nexura-a5e21",
-  storageBucket: "nexura-a5e21.appspot.com",
-  messagingSenderId: "45885832487",
-  appId: "1:45885832487:web:30126eb365cfb894c83d02",
-  measurementId: "G-HPNVD370MK",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
